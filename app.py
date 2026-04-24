00fa046a101744cbc1392b95fdb72cfab3c4d63d
TuNombre <tuemail@gmail.com>
2026-04-24 19:30:53 +0200

    mensaje

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Funciona 🚀"
+    return "Backend funcionando 🚀"
+
+@app.route("/saludo")
+def saludo():
+    return {"mensaje": "Hola desde Railway 🚀"}
+
+@app.route("/suma")
+def suma():
+    a = int(request.args.get("a", 0))
+    b = int(request.args.get("b", 0))
+    return {"resultado": a + b}
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
