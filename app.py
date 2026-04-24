49bfd413b9393a2028fd7dd7305ac7abc5493a05
TuNombre <tuemail@gmail.com>
2026-04-24 20:55:38 +0200

    fix cors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, send_from_directory, jsonify
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Carpeta para almacenar archivos
 UPLOAD_FOLDER = "uploads"
